cc49c8f671e973093b97b89832e1389085ca0128
malabasterr <113557081+malabasterr@users.noreply.github.com>
2024-04-14 15:36:45 +0100

    style homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Button, Stack } from 'react-bootstrap';
 
 function Home() {
   return (
-    <div>
-    <div>Out of the Abyss Allies</div>
-    <Link id="BruceLink" to="/Bruce">Bruce</Link>
-    <Link id="EnclaveLink" to="/Enclave">Emerald Enclave Scouts</Link>
-    <Link id="LizardsLink" to="/Lizards">Emerald Enclave Lizards</Link>
-    <Link id="GauntletLink" to="/Gauntlet">Gauntlet</Link>
-    <Link id="LordsAllianceLink" to="/LordsAlliance">Lords Alliance Guards</Link>
-    <Link id="LordsAllianceSpiesLink" to="/LordsAllianceSpies">Lords Alliance Spies</Link>
-    <Link id="HarpersLink" to="/Harpers">Harpers</Link>
+    <div className='HomeContainer'>
+      <div className='HomeBg'>
+      <div className="Title fw-bold HomeTitle text-center">Out of the Abyss Ally Groups</div>
+        <Stack gap={2} className="col-6 mx-auto">
+          <Button variant="dark" className="LinkBox" id="EnclaveLink" href="/Enclave">Emerald Enclave Scouts</Button>
+          <Button variant="dark" className="LinkBox" id="LizardsLink" href="/Lizards">Emerald Enclave Lizards</Button>
+          <Button variant="dark" className="LinkBox" id="LordsAllianceLink" href="/LordsAlliance">Lord's Alliance Guards</Button>
+          <Button variant="dark" className="LinkBox" id="LordsAllianceSpiesLink" href="/LordsAllianceSpies">Lord's Alliance Spies</Button>
+          <Button variant="dark" className="LinkBox" id="HarpersLink" href="/Harpers">Harper Rangers</Button>
+          <Button variant="dark" className="LinkBox" id="GauntletLink" href="/Gauntlet">Veterans of the Gauntlet</Button>
+          <Button variant="dark" className="LinkBox" id="BruceLink" href="/Bruce">Bruce (Shield Guardian)</Button>
+        </Stack>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
